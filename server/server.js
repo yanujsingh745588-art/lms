bb44590595e2ab78f278be0fe7baeaded5d44a4f
yanujsingh745588-art <yanujsingh745588@gmail.com>
2026-06-05 13:12:20 +0530

    Fix Clerk webhook route

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors())
 //Routes
 app.get('/',(req,res)=> res.send("Api working"))
 
-app.post('/clerk',express.json().clerkWebhooks)
+app.post('/clerk',express.json(),clerkWebhooks)
 // port 
 const PORT = process.env.PORT || 5000
 
