dc981af7920e078831a6235ff12229824a739e1c
yanujsingh745588-art <yanujsingh745588@gmail.com>
2026-06-05 13:40:21 +0530

    adreeses change

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -30,7 +30,7 @@ export const clerkWebhooks = async(req,res)=>{
                 
             case 'user.updated': {
                 const userData={
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name:data.first_name+ " " +data.last_name,
                     imageUrl : data.image_url,
                 }
